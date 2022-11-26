b7ebc18c0686c5f17a21ccec604b03aa5cdca5fa
SadraHeidariMoghadam <sadra_h_m@outlook.com>
2022-11-27 00:13:36 +0100

    Inspect Items Done. Fonts added.

---
+++ SleepInvasion/Assets/Scripts/Inventory&Items/InspectPanel.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InspectPanel : MonoBehaviour
+{
+    [SerializeField] private Button useButton;
+    [SerializeField] private TMP_Text description;
+    [SerializeField] private GameObject itemGameObject;
+
+    private GameController _gameController;
+
+
+    private void Start()
+    {
+        _gameController = GameController.Instance;
+    }
+
+    public void Setup(InteractableItemSO scriptableObject)
+    {
+        useButton.interactable = scriptableObject.usable;
+        useButton.onClick.RemoveAllListeners();
+        useButton.onClick.AddListener(() => _gameController.ItemsController.UseInventoryItem(scriptableObject.type));
+        description.text = scriptableObject.description;
+        itemGameObject.SetActive(true);
+        GameObject inspectGo = Instantiate(scriptableObject.prefab, itemGameObject.transform);
+        inspectGo.transform.localPosition = Vector3.zero;
+        inspectGo.AddComponent<Inspect>();
+        Destroy(inspectGo.GetComponent<Item>());
+        Destroy(inspectGo.GetComponent<Rigidbody>());
+    }
+    
+    public void Close()
+    {
+        if (itemGameObject.transform.childCount > 0)
+        {
+            var children = itemGameObject.GetComponentsInChildren<Inspect>();
+            {
+                foreach (var child in children)
+                {
+                    Destroy(child.gameObject);
+                }
+            }
+        }
+
+        itemGameObject.SetActive(false);
+        gameObject.SetActive(false);
+    }
+    
+}
--- SleepInvasion/Assets/Scripts/Inventory&Items/ItemUI.cs
+++ SleepInvasion/Assets/Scripts/Inventory&Items/ItemUI.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class ItemUI : MonoBehaviour
 {
+    public Button itemButton;
     public Image image;
     public TMP_Text count;
     public InteractableItemType type;
