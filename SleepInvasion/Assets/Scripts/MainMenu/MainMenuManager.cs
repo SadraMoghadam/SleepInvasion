092a6a37cdd65cb8f26c7338566edd3b9eb8196e
SadraHeidariMoghadam <sadra_h_m@outlook.com>
2022-12-04 23:32:57 +0100

    Added Feedback system. Added new materials to the scene.

--- SleepInvasion/Assets/Scripts/MainMenu/MainMenuManager.cs
+++ SleepInvasion/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -1,15 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] private TMP_Text title;
     [SerializeField] private Button resumeButton;
     [SerializeField] private Button newGameButton;
     [SerializeField] private Button quitButton;
+    [SerializeField] private Button feedBackButton;
+    [SerializeField] private GameObject feedbackPanel;
 
     private GameManager _gameManager;
+    
+    private static MainMenuManager _instance;
+    public static MainMenuManager Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+    }
+
     private void OnEnable()
     {
         Time.timeScale = 1;
@@ -21,6 +38,7 @@ public class MainMenuManager : MonoBehaviour
         resumeButton.onClick.AddListener(OnResumeClick);
         newGameButton.onClick.AddListener(OnNewGameClick);
         quitButton.onClick.AddListener(OnQuitClick);
+        feedBackButton.onClick.AddListener(OnFeedbackClick);
         _gameManager.AudioManager.play(SoundName.MainMenu);
     }
 
@@ -36,8 +54,29 @@ public class MainMenuManager : MonoBehaviour
         _gameManager.LoadScene("MainGame");
     }
     
+    private void OnFeedbackClick()
+    {
+        resumeButton.gameObject.SetActive(false);
+        newGameButton.gameObject.SetActive(false);
+        quitButton.gameObject.SetActive(false);
+        feedBackButton.gameObject.SetActive(false);
+        title.gameObject.SetActive(false);
+        feedbackPanel.SetActive(true);
+    }
+    
     private void OnQuitClick()
     {
         Application.Quit();
     }
+
+    public void CloseFeedback()
+    {
+        feedbackPanel.SetActive(false);
+        resumeButton.gameObject.SetActive(true);
+        newGameButton.gameObject.SetActive(true);
+        quitButton.gameObject.SetActive(true);
+        feedBackButton.gameObject.SetActive(true);
+        title.gameObject.SetActive(true);
+    }
+    
 }
