3912f62fb522fd639aef7cf402736b6914e2e2f0
DESKTOP-J49J2JD\giaco <giacomo.dellagosto@gmail.com>
2022-12-04 22:02:00 +0100

    please work

--- SleepInvasion/Assets/Scripts/Mechanics/DoorController.cs
+++ SleepInvasion/Assets/Scripts/Mechanics/DoorController.cs
@@ -1,33 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Mechanics;
 using UnityEngine;
 
-public class DoorController : MonoBehaviour
+public class DoorController : MonoBehaviour, IDoorController
 {
     [SerializeField] private bool startOpen;
 
     private Animator _doorAnimator;
+    
+    private static readonly int IsDoorOpen = Animator.StringToHash("isOpen");
 
     private void Awake()
     {
         _doorAnimator = GetComponent<Animator>();
-        _doorAnimator.SetBool("isOpen", startOpen);
+        _doorAnimator.SetBool(IsDoorOpen, startOpen);
     }
 
     public void Use()
     {
-        bool currentState = _doorAnimator.GetBool("isOpen");
-        _doorAnimator.SetBool("isOpen", !currentState);
+        bool currentState = _doorAnimator.GetBool(IsDoorOpen);
+        _doorAnimator.SetBool(IsDoorOpen, !currentState);
     }
 
     public void Close()
     {
-        _doorAnimator.SetBool("isOpen", false);
+        _doorAnimator.SetBool(IsDoorOpen, false);
     }
     
     public void Open()
     {
-        _doorAnimator.SetBool("isOpen", true);
+        _doorAnimator.SetBool(IsDoorOpen, true);
+    }
+
+    public bool IsOpen()
+    {
+        return _doorAnimator.GetBool(IsDoorOpen);
     }
 }
--- SleepInvasion/Assets/Scripts/Mechanics/ItemPlace.cs
+++ SleepInvasion/Assets/Scripts/Mechanics/ItemPlace.cs
@@ -7,11 +7,40 @@ public class ItemPlace : MonoBehaviour
 {
     public Item item;
     public Transform placementPosition;
-    [NonSerialized] public bool Empty;
+    
+    private bool _isEmpty;
+    private GameController _gameController;
     
     private void Awake()
     {
-        Empty = true;
+        _gameController = GameController.Instance;
+        _isEmpty = true;
+    }
+
+    public void SetEmpty()
+    {
+        _isEmpty = true;
+    }
+
+    public bool PlaceItem()
+    {
+        var tempItem = item.itemInfo.ItemScriptableObject;
+        if (!_isEmpty)
+        {
+            // TODO hint: there already is an item here
+            return false;
+        }
+        
+        if (!_gameController.InventoryController.IsItemInInventory(tempItem.type))
+        {
+            // TODO hint: the required object is not in the inventory
+            return false;
+        }
+        Instantiate(tempItem.prefab, placementPosition.position, placementPosition.rotation, gameObject.transform);
+        _isEmpty = false;
+        _gameController.InventoryController.DeleteInventoryData(tempItem.type);
+        
+        return true;
     }
 }
 
