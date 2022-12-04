4fd0de4c6d0a4fe3929809b6206186e19029ca93
DESKTOP-J49J2JD\giaco <giacomo.dellagosto@gmail.com>
2022-12-04 23:43:37 +0100

    added needle and sundial prefabs, fixed placement system, added door controller interface and double door

--- SleepInvasion/Assets/Scripts/Mechanics/DoubleDoorController.cs
+++ SleepInvasion/Assets/Scripts/Mechanics/DoubleDoorController.cs
@@ -1,34 +1,41 @@
-using Mechanics;
+﻿using Mechanics;
 using UnityEngine;
 
-public class DoorController : MonoBehaviour, IDoorController
+public class DoubleDoorController : MonoBehaviour, IDoorController
 {
+    [SerializeField] private GameObject otherDoor; 
     [SerializeField] private bool startOpen;
 
     private Animator _doorAnimator;
+    private Animator _otherDoorAnimator;
     
     private static readonly int IsDoorOpen = Animator.StringToHash("isOpen");
 
     private void Awake()
     {
         _doorAnimator = GetComponent<Animator>();
+        _otherDoorAnimator = otherDoor.GetComponent<Animator>();
         _doorAnimator.SetBool(IsDoorOpen, startOpen);
+        _otherDoorAnimator.SetBool(IsDoorOpen, startOpen);
     }
 
     public void Use()
     {
         bool currentState = _doorAnimator.GetBool(IsDoorOpen);
         _doorAnimator.SetBool(IsDoorOpen, !currentState);
+        _otherDoorAnimator.SetBool(IsDoorOpen, !currentState);
     }
 
     public void Close()
     {
         _doorAnimator.SetBool(IsDoorOpen, false);
+        _otherDoorAnimator.SetBool(IsDoorOpen, false);
     }
     
     public void Open()
     {
         _doorAnimator.SetBool(IsDoorOpen, true);
+        _otherDoorAnimator.SetBool(IsDoorOpen, true);
     }
 
     public bool IsOpen()
