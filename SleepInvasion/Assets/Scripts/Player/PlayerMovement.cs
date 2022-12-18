e77766613634e55236494e94754b1cbce005312a
SadraHeidariMoghadam <sadra_h_m@outlook.com>
2022-12-18 21:14:07 +0100

    Player Movement improvement. HeadBob.

--- SleepInvasion/Assets/Scripts/Player/PlayerMovement.cs
+++ SleepInvasion/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,15 +6,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [HideInInspector] public CharacterController controller;
-    [SerializeField] private float movementSpeed = 2f;
+    [HideInInspector] public Vector3 moveDirection = Vector3.zero;
+    [SerializeField] private float movementSpeed = 3f;
     [SerializeField] private float jumpHeight = 10f;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundDistance = 0.2f;
     [SerializeField] private LayerMask groundMask;
     
-    private float gravity = -9.81f;
+    private float gravity = 20f;
     private Vector3 _velocity;
     private bool _isGrounded;
+    [HideInInspector] public float vertical;
+    [HideInInspector] public float horizontal;
 
     private GameController _gameController;
 
@@ -29,28 +32,60 @@ public class PlayerMovement : MonoBehaviour
         {
             return;
         }
-        
-        // check if the player is on the ground to reset fall speed
-        _isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
-        if (Input.GetButtonDown("Jump") && _isGrounded)
+        if (!controller.isGrounded)
         {
-            _velocity.y = jumpHeight;
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        vertical = movementSpeed * Input.GetAxis("Vertical");
+        horizontal = movementSpeed * Input.GetAxis("Horizontal");
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * vertical) + (right * horizontal);
+
+        if (Input.GetButton("Jump") && controller.isGrounded)
+        {
+            moveDirection.y = jumpHeight;
         }
         else
         {
-            _velocity.y += gravity * Time.deltaTime;
+            moveDirection.y = movementDirectionY;
         }
-        controller.Move(_velocity * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+
+        // if (Cursor.lockState == CursorLockMode.Locked && canMove)
+        // {
+        //     Lookvertical = -Input.GetAxis("Mouse Y");
+        //     Lookhorizontal = Input.GetAxis("Mouse X");
+        //     rotationX += Lookvertical * lookSpeed;
+        //     rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        //     Camera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        //     transform.rotation *= Quaternion.Euler(0, Lookhorizontal * lookSpeed, 0);
+        //     if (isRunning && Moving) cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, RunningFOV, SpeedToFOV * Time.deltaTime);
+        //     else cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, InstallFOV, SpeedToFOV * Time.deltaTime);
+        // }
         
-        // move player through user input
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-
-        var cachedTransform = transform;
-        Vector3 move = cachedTransform.right * x + cachedTransform.forward * z;
-        controller.Move(movementSpeed * Time.deltaTime * move);
+        // // check if the player is on the ground to reset fall speed
+        // _isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        //
+        // if (Input.GetButtonDown("Jump") && _isGrounded)
+        // {
+        //     _velocity.y = jumpHeight;
+        // }
+        // else
+        // {
+        //     _velocity.y += gravity * Time.deltaTime;
+        // }
+        // controller.Move(_velocity * Time.deltaTime);
+        //
+        // // move player through user input
+        // float x = Input.GetAxis("Horizontal");
+        // float z = Input.GetAxis("Vertical");
+        //
+        //
+        // var cachedTransform = transform;
+        // Vector3 move = cachedTransform.right * x + cachedTransform.forward * z;
+        // controller.Move(movementSpeed * Time.deltaTime * move);
     }
 
     // private void OnCollisionEnter(Collision collision)
