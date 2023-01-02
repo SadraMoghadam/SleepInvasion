f2cf031c35b4d06df99dcd3a47e357d47f16d3b7
DESKTOP-J49J2JD\giaco <giacomo.dellagosto@gmail.com>
2023-01-02 16:32:24 +0100

    level 2 skeleton, small fixes, event system for level 2

---
+++ SleepInvasion/Assets/Scripts/Levels/Level2.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2 : Level
+{
+    private GameController _gameController;
+    private GameManager _gameManager;
+    private Level2Data _level2Data;
+
+    private float _gameTimer;
+
+    private int _processNumber;
+    
+    private void Awake()
+    {
+        _gameController = GameController.Instance;
+        _gameManager = GameManager.Instance;
+        _gameTimer = PlayerPrefsManager.GetFloat(PlayerPrefsKeys.GameTimer, 0);
+        _processNumber = PlayerPrefsManager.GetInt(PlayerPrefsKeys.Level1Process, 1);
+    }
+
+    public override int LevelNum => 2;
+
+    public override GameObject Self => _gameController.LevelsController.levelsDataContainer.level2Data.levelGO;
+    public override bool IsDone { get; protected set; }
+
+    public override void Setup()
+    {
+        _level2Data = _gameController.LevelsController.levelsDataContainer.level2Data;
+    }
+
+    public override void Process()
+    {
+        switch (_processNumber)
+        {
+            case 1:
+                FirstProcess();
+                break;
+        }
+    }
+
+    public void FirstProcess()
+    {
+        if (_level2Data.doubleDoorController.IsOpen())
+        {
+            SaveCompletedProcess(2);
+            EndOfLevel();
+        }
+    }
+
+    private void SaveCompletedProcess(int processNumber)
+    {
+        _processNumber = processNumber;
+        PlayerPrefsManager.SetInt(PlayerPrefsKeys.Level2Process, processNumber);
+    }
+
+    public override void EndOfLevel()
+    {
+        PlayerPrefsManager.SetInt(PlayerPrefsKeys.Level, 3);
+        IsDone = true;
+        PlayerPrefsManager.DeleteKey(PlayerPrefsKeys.Level2Process);
+    }
+}
---
+++ SleepInvasion/Assets/Scripts/Levels/Level2Data.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Level2Data
+{
+    public GameObject levelGO;
+    public CandlesPuzzle candlesPuzzle;
+    public DoubleDoorController doubleDoorController;
+}
