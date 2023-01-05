a414a9c9f2af72ad587d6bfdcb1c00d0b9ee6cce
Cloud_Li233 <libc233@gmail.com>
2023-01-05 05:29:45 +0100

    ++Add wood doors and metal gates, metal gate with locks. (but i can't fking make the script work, im stupid)  +Fix some material problems.

---
+++ SleepInvasion/Assets/Scripts/Mechanics/Lock/OpenGate.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class OpenGate : MonoBehaviour
+{
+    public int id;
+    private LockControl lockControl;
+    [SerializeField] private GameObject lockGO;
+    [SerializeField] private Animator animator;
+
+    void Awake()
+    {
+        lockControl = lockGO.GetComponent<LockControl>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefsKeys key = PlayerPrefsKeys.Chest1Unlocked;
+        switch (id)
+        {
+            case 1:
+                key = PlayerPrefsKeys.Chest1Unlocked;
+                break;
+            case 3:
+                key = PlayerPrefsKeys.Chest3Unlocked;
+                break;
+        }
+        if (PlayerPrefsManager.GetBool(key, false) && id == Int32.Parse(Regex.Match(key.ToString(), @"\d+").Value))
+        {
+            animator.Play("GateOpened");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lockControl.isOpened)
+        {
+            animator.SetBool("_open", true);
+        }
+    }
+}
